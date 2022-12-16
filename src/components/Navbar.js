b6224684966232031a8d422819eb65b92d938374
Azizur Rahman <47455161+AzizurRahmanEmon@users.noreply.github.com>
2022-12-16 15:19:03 +0000

    resume added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,13 @@ export default function Navbar() {
           </a>
         </nav>
         <a
+          href="https://docs.google.com/document/d/1ogH2iUekwIfZQhGdIdLJPB-I-ZoOND-Y/edit?usp=share_link&ouid=105730190292779357798&rtpof=true&sd=true"
+          target='_blank'
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          Resume
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a>
+        <a
           href="#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Contact Me
