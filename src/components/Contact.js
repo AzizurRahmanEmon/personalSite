67d100969ba595e01852665b9d7b994eff96dd3f
Azizur Rahman <azizurrahmanemon007@gmail.com>
2023-02-05 06:52:53 -0800

    Update Contact.js
    email updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,7 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                aremon2533@email.com
+                azizur15-2533@diu.edu.bd
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
